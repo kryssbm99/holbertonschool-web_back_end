1ba1150da706f9dded31f2a073b6becc4ee96431
Kryss <kryssbm99@gmail.com>
2024-07-16 22:31:01 -0400

    Final fix

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,15 @@
+// Export the default function appendToEachArrayValue
 export default function appendToEachArrayValue(array, appendString) {
+    // Initialize an empty array to store the new values
     const newArray = [];
+    
+    // Use a for...of loop to iterate over each element in the array
     for (const idx of array) {
+      // Append the given string to the current element and push it to the new array
       newArray.push(appendString + idx);
     }
   
+    // Return the new array with the modified values
     return newArray;
   }
+  
