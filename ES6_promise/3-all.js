418ea2ef17316203d7bb99dbccd499b7c1d4091c
Kryss <kryssbm99@gmail.com>
2024-07-25 16:54:59 -0400

    Task #2-3

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,18 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+/**
+ * Handles the profile signup by resolving all promises
+ * and logging the required information or handling errors
+ */
+function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const [photoResponse, userResponse] = results;
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
+
+export default handleProfileSignup;
