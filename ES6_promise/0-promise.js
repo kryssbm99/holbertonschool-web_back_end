4d6496b7e729c8bddd976f7ab2857992b5493169
Kryss <kryssbm99@gmail.com>
2024-07-25 16:29:44 -0400

    Fixes

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -3,12 +3,12 @@
  * @returns {Promise}
  */
 function getResponseFromAPI() {
-    // Returning a new promise
-    return new Promise((resolve, reject) => {
-        // The executor function inside the promise can have some logic
-        // In this case, we're simply resolving the promise
-        resolve('Response from API');
-    });
+  // Returning a new promise
+  return new Promise((resolve) => {
+    // The executor function inside the promise can have some logic
+    // In this case, we're simply resolving the promise
+    resolve('Response from API');
+  });
 }
 
 // Export the function
