073b57f6bbdeb2d9f99822cf908f183bf38b3f18
Kryss <kryssbm99@gmail.com>
2024-09-19 05:01:23 -0400

    Pycode fix

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,8 +4,10 @@ import asyncio
 import random
 from typing import Generator
 
+
 async def async_generator() -> Generator[float, None, None]:
-    """Asynchronous generator that waits for 1 second and yields a random float between 0 and 10."""
+    """Asynchronous generator that waits for 1 second
+    and yields a random float between 0 and 10."""
     for _ in range(10):
         await asyncio.sleep(1)  # Pause execution for 1 second
-        yield random.uniform(0, 10)  # Yield a random float within the range [0, 10]
+        yield random.uniform(0, 10)
---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Async comprehension task 1"""
+import asyncio
+from typing import List
+
+task_0_function = __import__('0-async_generator').async_generator
+
+async def async_comprehension() -> List[float]:
+    """Asynchronous function to collect and return 10 random numbers from the async generator."""
+    return [i async for i in task_0_function()]  # Collects the generated numbers asynchronously
