b35ee97567eca23c776b2381a361cdf044e3a913
Kryss <kryssbm99@gmail.com>
2024-09-19 05:08:52 -0400

    First commit task #0

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Simple function for pagination."""
+from typing import Tuple
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Returns a tuple containing the start and end indexes 
+    for the given page and page size.
+    """
+    start = (page - 1) * page_size  # Calculate the starting index
+    end = page * page_size  # Calculate the ending index
+    return (start, end)  # Return the start and end indexes
