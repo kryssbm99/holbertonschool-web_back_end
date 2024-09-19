f4bc71da8d0b859f6b15f942b10b19a3364fe901
Kryss <kryssbm99@gmail.com>
2024-09-19 04:58:11 -0400

    Task#0 and README file

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Async generator task 0"""
+import asyncio
+import random
+from typing import Generator
+
+async def async_generator() -> Generator[float, None, None]:
+    """Asynchronous generator that waits for 1 second and yields a random float between 0 and 10."""
+    for _ in range(10):
+        await asyncio.sleep(1)  # Pause execution for 1 second
+        yield random.uniform(0, 10)  # Yield a random float within the range [0, 10]
